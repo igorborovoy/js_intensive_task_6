ad814ba8e0c827c07e6dc76df516741a315beff8
Igor Borovoy <igorborovoy@yandex.by>
2021-01-13 00:35:13 +0300

    fixed range of fibonacci

--- task_6_1.js
+++ task_6_1.js
@@ -1,5 +1,5 @@
 function calcSumFromFibonacci(from, to) {
-    
+
     if (!Number.isFinite(from) || !Number.isFinite(to) || from < 0 || from >= to) {
         throw new Error('Incorrect input');
     }
@@ -9,7 +9,7 @@ function calcSumFromFibonacci(from, to) {
     arr[0] = 0;
     arr[1] = 1;
 
-    for (let i = 2; i < 10; i++) {
+    for (let i = 2; i < to + 1; i++) {
         arr[i] = arr[i - 1] + arr[i - 2];
     }
 
